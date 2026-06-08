62a6e1dbac80e0ffa79dfa056a00a9ee4935d711
PresilskiAleksandar <aleksandarpresilski@gmail.com>
2026-06-08 13:59:09 +0200

    Set production API URL as default in config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://medinfo-backend-o36l.onrender.com/api';
 export const IMAGE_BASE = API_URL.replace('/api', '');
 export default API_URL;
